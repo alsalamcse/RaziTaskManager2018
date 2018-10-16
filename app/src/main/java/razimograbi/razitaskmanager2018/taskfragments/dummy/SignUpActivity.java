2935463b77933c053f4ff515ffee5902ae84f126
user <tasnem.mobarky@gmail.com>
2018-10-16 13:21:17 +0300

    yo

--- app/src/main/java/razimograbi/razitaskmanager2018/taskfragments/dummy/SignUpActivity.java
+++ app/src/main/java/razimograbi/razitaskmanager2018/taskfragments/dummy/SignUpActivity.java
@@ -9,15 +9,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import razimograbi.razitaskmanager2018.R;
 
 public class SignUpActivity extends AppCompatActivity {
     EditText firstName , LastName , Phone , Email , Password;
     Button Save;
+
+     FirebaseAuth auth; //to establish sign in sign up
+     FirebaseUser user; //user
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        auth = FirebaseAuth.getInstance();
+
+        user = auth.getCurrentUser(); // return if user has signed in  if not return null
+
         firstName = (EditText)findViewById(R.id.editText);
         LastName = (EditText)findViewById(R.id.editText2);
         Phone = (EditText)findViewById(R.id.PhoneId);
@@ -31,6 +42,7 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
     }
+
 
 
 }
