73b29b362b4c17dd8e954765cb5dad16d57bae6b
user <tasnem.mobarky@gmail.com>
2018-12-06 14:05:56 +0200

    yo

--- app/src/main/java/razimograbi/razitaskmanager2018/taskfragments/MyTasksFragment.java
+++ app/src/main/java/razimograbi/razitaskmanager2018/taskfragments/MyTasksFragment.java
@@ -62,18 +62,22 @@ public class MyTasksFragment extends Fragment {
             } else {
                 recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
             }
-            recyclerView.setAdapter(new MyItemRecyclerViewAdapter(DummyContent.ITEMS, mListener));
+            recyclerView.setAdapter(new MyItemRecyclerViewAdapter(readTasks(), mListener));
         }
         return view;
     }
 
     private List<MyTask> readTasks(){
-        ArrayList<MyTask> myTasks = null;
+        final ArrayList<MyTask> myTasks = new ArrayList<>();
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference(); // take reference to the first
         reference.child("MyTasks").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
+                for (DataSnapshot d : dataSnapshot.getChildren()){
+                    MyTask task = d.getValue(MyTask.class);
+                    myTasks.add(task);
+                }
             }
 
             @Override
@@ -112,6 +116,6 @@ public class MyTasksFragment extends Fragment {
      */
     public interface OnListFragmentInteractionListener {
         // TODO: Update argument type and name
-        void onListFragmentInteraction(DummyItem item);
+        void onListFragmentInteraction(MyTask item);
     }
 }
